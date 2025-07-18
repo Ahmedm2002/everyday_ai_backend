438f4026f587bec07679745e559547d9c25263a3
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-07-18 13:07:45 +0500

    added google genai package

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import connectDb from "./src/configs/db.js";
 import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+import chatRoutes from "./src/routes/chat.routes.js";
+import userRoutes from "./src/routes/user.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,6 +12,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 connectDb();
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+  apiVersion: "v1alpha",
+});
+
+app.use(
+  cors({
+    origin: true,
+  })
+);
+
+console.log("Ai: ", ai);
+
+app.use("/chat", chatRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log("Server started");
