78a45c38ae151da35ccd9d615d95c07c763516c0
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-12-13 23:37:02 +0500

    added get route at home

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { corsOptions } from "./configs/cors.js";
 import router from "./routes/index.js";
+import API_RES from "./utils/ApiRes.js";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 
 app.use("/api", router);
+app.get('/', (req, res) => {
+    res.status(200).json(new API_RES(true, 200, "Backend Working fine", null, []))
+})
 
 export default app;
